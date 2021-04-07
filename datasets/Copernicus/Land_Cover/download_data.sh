277e7da8e5dcf0b07a5b0fbdf90a8cb36ea020ab
John Rattz <john.c.rattz@ama-inc.com>
2021-04-07 13:29:07 -0400

    Added some more information about why Change_Confidence_layer is commented out as a measurement in Copernicus/Land_Cover/prod_def.yaml.

---
+++ datasets/Copernicus/Land_Cover/download_data.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+aws s3 cp s3://vito.landcover.global/v3.0.1/2015/W080N40/ vito.landcover.global/v3.0.1/2015/W080N40/ --no-sign-request --recursive
+aws s3 cp s3://vito.landcover.global/v3.0.1/2015/W100N40/ vito.landcover.global/v3.0.1/2015/W100N40/ --no-sign-request --recursive
+
+aws s3 cp s3://vito.landcover.global/v3.0.1/2016/W080N40/ vito.landcover.global/v3.0.1/2016/W080N40/ --no-sign-request --recursive
+aws s3 cp s3://vito.landcover.global/v3.0.1/2016/W100N40/ vito.landcover.global/v3.0.1/2016/W100N40/ --no-sign-request --recursive
+
+aws s3 cp s3://vito.landcover.global/v3.0.1/2017/W080N40/ vito.landcover.global/v3.0.1/2017/W080N40/ --no-sign-request --recursive
+aws s3 cp s3://vito.landcover.global/v3.0.1/2017/W100N40/ vito.landcover.global/v3.0.1/2017/W100N40/ --no-sign-request --recursive
+
+aws s3 cp s3://vito.landcover.global/v3.0.1/2018/W080N40/ vito.landcover.global/v3.0.1/2018/W080N40/ --no-sign-request --recursive
+aws s3 cp s3://vito.landcover.global/v3.0.1/2018/W100N40/ vito.landcover.global/v3.0.1/2018/W100N40/ --no-sign-request --recursive
+
+aws s3 cp s3://vito.landcover.global/v3.0.1/2019/W080N40/ vito.landcover.global/v3.0.1/2019/W080N40/ --no-sign-request --recursive
+aws s3 cp s3://vito.landcover.global/v3.0.1/2019/W100N40/ vito.landcover.global/v3.0.1/2019/W100N40/ --no-sign-request --recursive
